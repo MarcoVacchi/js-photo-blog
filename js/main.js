3354655df1e07eb1e2e2c3bb12d9f7afa383ed33
MarcoVacchi <mvacchi816@gmail.com>
2025-03-14 16:41:03 +0100

    feat: create readme

--- js/main.js
+++ js/main.js
@@ -22,24 +22,24 @@ Buon Lavoro e buon divertimento!*/
 
 
 const endPoint = "https://lanciweb.github.io/demo/api/pictures/"; //creo una variabile che aggancia il server
-const container= document.getElementById("container-photo");
+const container = document.getElementById("container-photo"); // salvo in una variabile 
 let myPhoto = '';
 //console.log(endPoint);
 
 axios.get(endPoint)
-.then(response =>{
+    .then(response => {
 
-    myPhoto = response.data;
-    let images = '';
+        myPhoto = response.data;
+        let images = '';
 
         //console.log(myPhoto);
         //console.log(response.data);
-    myPhoto.forEach(result => {
-        console.log(result.url)
-        //console.log(result.url);
-        //console.log(result);
+        myPhoto.forEach(result => {
+            console.log(result.url)
+            //console.log(result.url);
+            //console.log(result);
 
-        images += `<div class="card col-lg-4 col-md-6 col-sm-12" style="width: 18rem;">
+            images += `<div class="card col-lg-4 col-md-6 col-sm-12" style="width: 18rem;">
                     <div class="p-4 position-static bg-light">
                         <div class="position-absolute top-0 start-50 translate-middle">
                             <img src="img/pin.svg" alt="puntine">
@@ -50,10 +50,10 @@ axios.get(endPoint)
                         <p class="card-text">${result.title}</p>
                         <p class="card-text">${result.date}</p>
                     </div>
-                </div>` 
-    });
-    
-    container.innerHTML = images;
-        
+                </div>`
+        });
+
+        container.innerHTML = images;
+
     });
 
